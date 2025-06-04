d78730f9ced47937a6434364dbb7d5a3e692d80b
Jason Padilla <84742412+Jayleaf1125@users.noreply.github.com>
2025-06-03 21:28:24 -0400

    Save Ability Works

---
+++ Assets/Scripts/SavePosition.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class SavePosition : MonoBehaviour
+{
+    Vector3 PosSave;
+    bool IsPositionBeingSaved;
+    public GameObject SaveLocationPrefab;
+    GameObject LocationObj;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        IsPositionBeingSaved = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space) && !IsPositionBeingSaved)
+        {
+            SavePos();
+        } else if (Input.GetKeyUp(KeyCode.Space) && IsPositionBeingSaved)
+        {
+            LoadPos();
+        }
+    }
+
+    void SavePos()
+    {
+        Debug.Log("Save Position");
+        PosSave = new Vector3(this.transform.position.x, this.transform.position.y, 0);
+        LocationObj = Instantiate(SaveLocationPrefab, PosSave, Quaternion.identity);
+        IsPositionBeingSaved = true;
+    }
+
+    void LoadPos()
+    {
+        Debug.Log("Load Position");
+        this.transform.position = PosSave;
+        Destroy(LocationObj);
+        PosSave = Vector3.zero;
+        IsPositionBeingSaved = false;
+    }
+
+
+}
